95b1c96ef8a49a218ae002232c8b9cfca93b718f
DanuZz ? <danuxz01@gmail.com>
2026-02-01 15:50:06 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 // config.js
 module.exports = {
-    telegram_token: "7930849426:AAHdt_R_AtyGpFAkcHQo1fQNFLtuROax35o",   // ← put your real token here
-
-    // You can add more settings later
+    telegram_token: "7930849426:AAHdt_R_AtyGpFAkcHQo1fQNFLtuROax35o",
     bot_name: "Danuxxxii Botz",
     owner_name: "@Danu'Zz",
     contact_number: "+94766911711",
-
-    // optional – if you later want to disable AI temporarily
     ai_chat_enabled: true,
+
+    // MongoDB
+    MONGO_URI: 'mongodb+srv://danuz_movanest:danuz_movanest@cluster0.3pm9uqz.mongodb.net/',
+    MONGO_DB: 'Telegram_Ai_Chat_Bot',
 };
