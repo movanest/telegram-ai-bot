dfb0b2b9db8ccd95546bce704268e13d13d2b74f
DanuZz ? <danuxz01@gmail.com>
2026-02-01 17:36:22 +0530

    @Danu'Zz Free Telegram Ai Chat Bot Base 🎀
    @Danu'Zz is a free AI chat Telegram bot for smart replies, ideas, and instant help.

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 // config.js
 module.exports = {
-    telegram_token: "7930849426:AAHdt_R_AtyGpFAkcHQo1fQNFLtuROax35o",   // ← put your real token here
+    telegram_token: "xxxx",   // ← put your real token here
 
     // You can add more settings later
     bot_name: "Danuxxxii Botz",
     owner_name: "@Danu'Zz",
-    contact_number: "+94766911711",
+    contact_number: "+9476XXXXX",
 
     // optional – if you later want to disable AI temporarily
     ai_chat_enabled: true,
