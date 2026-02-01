53e9b23b7174d247d2e64fda36043ba7df048d02
@MovaNest <mova.nest@gmail.com>
2026-02-01 20:21:36 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 // config.js
 module.exports = {
-    telegram_token: "xxxx",   // ← put your real token here
+    telegram_token: "8472816283:AAEmTjsctTdFTDVnW8Op2shOwctd8ZiGrPw",   // ← put your real token here
 
     // You can add more settings later
     bot_name: "Danuxxxii Botz",
     owner_name: "@Danu'Zz",
-    contact_number: "+9476XXXXX",
+    contact_number: "+94766911711",
 
     // optional – if you later want to disable AI temporarily
     ai_chat_enabled: true,
